1503f8f64a87a231cfed0e49696a1d25c73e8f86
chad1908 <34282981+chad1908@users.noreply.github.com>
2021-04-24 15:34:58 -0400

    Smoother movement/Added ability to jump
    The movement that was first implemented was just a rough place holder for the time. Now the movement is much more smooth and feels a lot better.

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -6,8 +6,15 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
+    public bool isJumping;
+    public float jumpSpeed = 8f;
+    public float jumpDurationThreshold = 0.25f;
     public float speed = 14f;
     public float accel = 6f;
+    private float jumpDuration;
+    private float rayCastLengthCheck = 0.05f;
+    private float width;
+    private float height;
     private Vector2 input;
     private SpriteRenderer sr;
     private Rigidbody2D rb;
@@ -18,15 +25,42 @@ public class MovementController : MonoBehaviour
         sr = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+
+        //Grabs the width and height of the sprite and adds some padding.
+        width = GetComponent<Collider2D>().bounds.extents.x + 0.1f;
+        height = GetComponent<Collider2D>().bounds.extents.y + 0.2f;
+    }
+
+    public bool PlayerIsGrounded()
+    {
+        //casts 3 rays under the player sprite to make sure the base of the sprite is covered, and gives the sprite realistic boundaries.
+        bool groundCheck1 = Physics2D.Raycast(new Vector2
+            (transform.position.x, transform.position.y - height), -Vector2.up, rayCastLengthCheck);
+        bool groundCheck2 = Physics2D.Raycast(new Vector2
+            (transform.position.x + (width - 0.2f),
+            transform.position.y - height), -Vector2.up, rayCastLengthCheck);
+        bool groundCheck3 = Physics2D.Raycast(new Vector2
+            (transform.position.x - (width - 0.2f),
+            transform.position.y - height), -Vector2.up, rayCastLengthCheck);
+
+        //Returns whether the raycast is being triggered or not.
+        if (groundCheck1 || groundCheck2 || groundCheck3)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }   
     }
 
     private void Update()
     {
-        //Gets these values from unity
+        //Gets these values from unity.
         input.x = Input.GetAxis("Horizontal");
         input.y = Input.GetAxis("Jump");
 
-        //Determines which way the sprite should be facing according to the x axis
+        //Determines which way the sprite should be facing according to the x axis.
         if (input.x > 0f)
         {
             sr.flipX = false;
@@ -35,6 +69,29 @@ public class MovementController : MonoBehaviour
         {
             sr.flipX = true;
         }
+
+        //as long as jump is held time.deltaTime will increment jump duration.
+        if (input.y >=1f)
+        {
+            jumpDuration += Time.deltaTime;
+        }
+        else
+        {
+            isJumping = false;
+            jumpDuration = 0f;
+        }
+
+        //Checks to see if the player is grounded or jumping.
+        if (PlayerIsGrounded() && isJumping == false)
+        {
+            if (input.y > 0f)
+            {
+                isJumping = true;
+            }
+        }
+
+        //If the player holds jump longer than the threshold the input is set to 0.
+        if (jumpDuration > jumpDurationThreshold) input.y = 0f;
     }
 
     private void FixedUpdate()
@@ -56,6 +113,12 @@ public class MovementController : MonoBehaviour
         rb.AddForce(new Vector2(((input.x * speed) - rb.velocity.x) * acceleration, 0));
         //Controls the player velocity.
         rb.velocity = new Vector2(xVelocity, rb.velocity.y);
+
+        //
+        if (isJumping && jumpDuration < jumpDurationThreshold)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
     }
 
 }
