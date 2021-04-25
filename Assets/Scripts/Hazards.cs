be34a0585814c986a32b1827bbdd4cef3ff092cb
chad1908 <34282981+chad1908@users.noreply.github.com>
2021-04-25 16:49:42 -0400

    Added Particle system for player destroy
    Made a new particle that is an explosion of red, this will be used when the player is destroyed.

---
+++ Assets/Scripts/Hazards.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazards : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Check to make sure the colliding object is the player
+        if (collision.transform.tag == "Player")
+        {
+            //Destroy the colliding object
+            Destroy(collision.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
