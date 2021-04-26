94d0933a7d105db6cff7a54c9cc499a42c95fa06
chad1908 <34282981+chad1908@users.noreply.github.com>
2021-04-26 12:27:24 -0400

    Hazard script/Death Particles
    Added a hazard script that will destroy the player prefab on collision with a game object, when the player prefab is destroyed a particle system is spawned on the players position that shoots red particles upwards.

--- Assets/Scripts/Hazards.cs
+++ Assets/Scripts/Hazards.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class Hazards : MonoBehaviour
 {
+    public GameObject playerDeathParticles;
+    private SpriteRenderer spriteRenderer;
+
+    //Caches a reference to the sprite renderer when the script starts.
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         //Check to make sure the colliding object is the player
         if (collision.transform.tag == "Player")
         {
+            //instantiate the death particles at the point of collision.
+            Instantiate(playerDeathParticles, collision.contacts[0].point, Quaternion.identity);
+
             //Destroy the colliding object
             Destroy(collision.gameObject);
         }
