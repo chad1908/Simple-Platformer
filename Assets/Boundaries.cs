a68720756eaab7614905ca5fca8850035fe94fa9
chad1908 <34282981+chad1908@users.noreply.github.com>
2021-05-25 22:33:17 -0400

    Added player boundaries script
    Made a boundaries script that will be used to make boundaries that the player cannot pass, this will help prevent the player from falling off the map.

---
+++ Assets/Boundaries.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundaries : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            GameObject player = GameObject.Find("Player");
+            MovementController movementController = player.GetComponent<MovementController>();
+            movementController.jump = 0;
+            Debug.Log("jump set to 0");
+        }
+    }
+}
