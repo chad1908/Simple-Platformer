e6c21e06ef4bb360be1217f8d57cce0de6bd1dac
chad1908 <34282981+chad1908@users.noreply.github.com>
2021-06-13 15:57:44 -0400

    arrowSpawner/Level design
    Added a new arrowSpawner prefab and arrow prefab, these are used as traps in the game. Did some more work on level 2 adding a new section using the arrowSpawner.

---
+++ Assets/Scripts/arrowScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrowScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public Vector2 direction = new Vector2(0, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = direction;
+
+        StartCoroutine(destroyArrow());
+    }
+
+    IEnumerator destroyArrow()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(GameObject.FindWithTag("Arrow"));
+    }
+}
---
+++ Assets/arrowSpawner.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrowSpawner : MonoBehaviour
+{
+    public arrowScript arrowPrefab;
+    public float arrowSpeed;
+    private float time = 0;
+    public float arrowDelay;
+
+    public bool shootLeft = false;
+    public bool shoot;
+
+    private Vector2 dir2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dir2 = new Vector2(arrowSpeed, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shoot == true)
+        {
+            if (time < Time.time)
+            {
+                arrowScript arrow = Instantiate(arrowPrefab, transform.position, transform.rotation) as arrowScript;
+                arrow.direction = dir2;
+                if (shootLeft == true)
+                {
+                    arrow.transform.rotation = Quaternion.Euler(0, 0, 90);
+                }
+                else
+                {
+                    arrow.transform.rotation = Quaternion.Euler(0, 0, -90);
+                }
+                time = Time.time + arrowDelay;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            shoot = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            shoot = false;
+        }
+    }
+}
